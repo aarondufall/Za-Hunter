8d2e11f06869cd6f07c800595dfc3235a3251840
Aaron Dufall <aald212@gmail.com>
2014-03-26 12:38:25 -0700

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Aaron Dufall on 26/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZaHunter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZaHunter
+//
+//  Created by Aaron Dufall on 26/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
